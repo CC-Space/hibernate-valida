efc8a7eae2fe99cc11951716ba4c835777725a3f
Chris Cai <361389383@qq.com>
2018-03-21 09:30:44 +0800

    更新对象级联关系demo

---
+++ src/main/java/com/valida/example/demo/domain/Father.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2005-2018 , FPX and/or its affiliates. All rights reserved.
+ * Use, Copy is subject to authorized license.
+ */
+package com.valida.example.demo.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Father {
+    @NotEmpty
+    private String name;
+}
--- src/main/java/com/valida/example/demo/domain/Student.java
+++ src/main/java/com/valida/example/demo/domain/Student.java
@@ -2,7 +2,9 @@ package com.valida.example.demo.domain;
 
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Student {
@@ -12,4 +14,8 @@ public class Student {
 
     @NotEmpty(message = "不能为空2")
     private String age;
+
+    @Valid
+    @NotNull
+    private Father father;
 }
